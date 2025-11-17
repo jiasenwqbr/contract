d928b0e0991c38fdeecfeaf05e3f69f300abfe70
Jason <jasonjiasen@gmail.com>
2025-11-17 11:51:29 +0800

    StakingRewardDistribute

--- contracts/pijsStaking/StakingRewardDistribute.sol
+++ contracts/pijsStaking/StakingRewardDistribute.sol
@@ -50,47 +50,124 @@ contract StakingRewardDistribute is
         );
     }
 
-        /*//////////////////////////////////////////////////////////////
-                            STATE VARIABLES
-        /////////////////////////////////////////////////////////////*/
-        mapping(address => mapping(uint256 => Reward)) public rewards;
+    /*//////////////////////////////////////////////////////////////
+                        STATE VARIABLES
+    /////////////////////////////////////////////////////////////*/
+    mapping(uint256 =>  WithdrawRewardOrder) public withdrawRewardOrders;
+    mapping(address => uint256[] ) public userWithdrawRewardOrderIds;
+
+    bytes32 private constant PERMIT_WITHDRAWREWARD_TYPEHASH = keccak256(
+        abi.encodePacked(
+            "Permit(uint256 orderId,uint256 amount,address beneficiaryAddress,uint256 nonce)"
+        )
+    );
+    mapping(address => uint) public withdrawNonces;
+    mapping(uint256 => uint256) public rewardRecords;
+
+    /*//////////////////////////////////////////////////////////////
+                            Struct
+    //////////////////////////////////////////////////////////////*/
+    struct WithdrawRewardOrder{
+        uint256 orderId;
+        uint256 amount;
+        address beneficiaryAddress;
+        uint256 withdrawTime;
+        uint256 nonce;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                EVENTS
+    //////////////////////////////////////////////////////////////*/
+    event WithdrawReward(uint256 orderId,uint256 amount,address beneficiaryAddress,uint256 nonce,uint256 withdrawTime);
+    event GenerateRewards(address operator,uint256 yyyymmdd,uint256 amount,uint256 amountSum,uint256 createTime);
+    /*//////////////////////////////////////////////////////////////
+                            MODIFIERS
+    //////////////////////////////////////////////////////////////*/
 
 
+    /*//////////////////////////////////////////////////////////////
+                            FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function generateRewards(uint256 yyyymmdd) public onlyRole(OPERATE_ROLE) nonReentrant payable {
+        require(msg.value > 0,"StakingRewardDistribute:msg.value should > 0");
+        rewardRecords[yyyymmdd] = rewardRecords[yyyymmdd] + msg.value;
+        
+        emit GenerateRewards(msg.sender,yyyymmdd,msg.value,rewardRecords[yyyymmdd],block.timestamp);
+    }
 
-        /*//////////////////////////////////////////////////////////////
-                               Struct
-        //////////////////////////////////////////////////////////////*/
-        struct Reward {
-            uint256 yyyymm;
-            address beneficiaryAddress;
-            uint256 amount;
-            uint256 createTime;
-            bool isWithdraw;
-            uint256 withdrawTime;
+    function withdrawReward(bytes memory data) public nonReentrant {
+        WithdrawRewardOrder memory order = parseWithdrawReward(data);
+        require(order.nonce == withdrawNonces[msg.sender],"StakingRewardDistribute:INVALID_NONCE");
+        require(msg.sender == order.beneficiaryAddress,"StakingRewardDistribute:invalid user address");
+        require(order.beneficiaryAddress != address(0),"StakingRewardDistribute:address 0 is not allowed");
+        require(withdrawRewardOrders[order.orderId].beneficiaryAddress != address(0),"StakingRewardDistribute:withwarded");
+
+        withdrawRewardOrders[order.orderId] = order;
+        userWithdrawRewardOrderIds[msg.sender].push(order.orderId);
+        withdrawNonces[msg.sender]++;
+
+        (bool success, ) = payable(msg.sender).call{value: order.amount }("");
+        require(success, "StakingRewardDistribute:Native  transfer failed");
+
+        emit WithdrawReward(order.orderId,order.amount,order.beneficiaryAddress,order.nonce,order.withdrawTime);
+    }
+
+    function parseWithdrawReward(bytes memory data) internal view returns (WithdrawRewardOrder memory) {
+        (
+            uint256 orderId,
+            uint256 amount,
+            address beneficiaryAddress,
+            uint256 nonce,
+            bytes memory signature
+        ) = abi.decode(
+            data,
+            (
+                uint256,
+                uint256,
+                address,
+                uint256,
+                bytes
+            )
+        );
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(signature);
+        bytes32 signHash = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(
+                    abi.encode(
+                        PERMIT_WITHDRAWREWARD_TYPEHASH,
+                        orderId,
+                        amount,
+                        beneficiaryAddress,
+                        nonce
+                    )
+                )
+            )
+        );
+        require(signer == ecrecover(signHash, v, r, s),"StakingRewardDistribute:INVALID_REQUEST");
+        return WithdrawRewardOrder({
+            orderId:orderId,
+            amount:amount,
+            beneficiaryAddress:beneficiaryAddress,
+            withdrawTime:block.timestamp,
+            nonce:nonce
+        });
+    }
+
+    function splitSignature(
+        bytes memory sig
+    ) internal pure returns (uint8, bytes32, bytes32) {
+        require(sig.length == 65, "StakingRewardDistribute:Not Invalid Signature Data");
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
         }
-
-
-        /*//////////////////////////////////////////////////////////////
-                                 EVENTS
-        //////////////////////////////////////////////////////////////*/
-
-        /*//////////////////////////////////////////////////////////////
-                               MODIFIERS
-        //////////////////////////////////////////////////////////////*/
-
-
-        /*//////////////////////////////////////////////////////////////
-                               FUNCTIONS
-        //////////////////////////////////////////////////////////////*/
-        function generateRewards() public onlyRole(OPERATE_ROLE) {
-
-        }
-
-        function withdrawReward() public nonReentrant {
-
-        }
-
-
-
+        return (v, r, s);
+    }
 
 }
